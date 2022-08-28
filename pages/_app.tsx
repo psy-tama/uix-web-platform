b97c2ace97b9b72deb7dbc93cde7fc987b2179f6
psy-tama <112111595+psy-tama@users.noreply.github.com>
2022-08-28 21:12:21 +0530

    UIX-11: Added config to use absolute path, no-relative-parent-import rule (#11)
    feat: Added ability to use absolute path, no relative path rule

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import GlobalStyle from '../global/globalstyles';
+import GlobalStyle from 'global/globalstyles';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
