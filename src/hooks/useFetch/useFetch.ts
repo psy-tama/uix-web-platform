4740a0efff5c9a6db52248c8131bd3a3c45fcc73
Psy <psy@uixlabs.in>
2022-09-03 13:32:42 +0530

    chore: added usefetch documentation

--- src/hooks/useFetch/useFetch.ts
+++ src/hooks/useFetch/useFetch.ts
@@ -1,10 +1,23 @@
-import useSWR, { Key, Fetcher, SWRResponse } from 'swr';
+import useSWR, { Key, Fetcher, SWRResponse, SWRConfiguration } from 'swr';
 import { sanitizeOptions } from './helper';
+
+/***
+ * url: absolute url of the API e.g https://dev.com/user?include=details
+ *
+ * options: request options to pass to fetch. body, header and other config go inside this.
+ * See https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch#supplying_request_options
+ *
+ * configOverride: options to override default SWR config.
+ * See https://swr.vercel.app/docs/options#options
+ *
+ * enabled: boolean flag to conditionally enable/disable fetching.
+ * usefetch will not fetch the passed url if enabled is passed false.
+ */
 
 interface FetchArgTypes {
   url: string;
   options?: RequestInit;
-  configOverride?: RequestInit;
+  configOverride?: SWRConfiguration;
   enabled?: boolean;
 }
 
